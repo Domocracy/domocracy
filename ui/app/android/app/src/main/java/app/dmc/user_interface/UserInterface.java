388cb79df5e663e82f67fc9b66a53c5e70918034
bardo91 <19Bardo91@gmail.com>
2015-02-10 16:31:27 +0100

    Basic ui classes and interfaces

---
+++ ui/app/android/app/src/main/java/app/dmc/user_interface/UserInterface.java
@@ -0,0 +1,45 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//      Domocracy Android App
+//          Author: Pablo R.S.
+//         Date:    2015-FEB-10
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+
+package app.dmc.user_interface;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import app.dmc.Hub;
+import app.dmc.R;
+
+public class UserInterface {
+    //-----------------------------------------------------------------------------------------------------------------
+    //  Public Interface
+    public UserInterface(Activity _activity){
+        mMainScreen = new MainScreen(_activity);
+        mLateralMenu = new SlideMenu(_activity);
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------
+    public void setHub(Hub _hub){
+        mMainScreen.set(_hub);
+        mLateralMenu.set(_hub);
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------
+    public View build(Activity _activity){
+        // Dummy build
+        LayoutInflater inflater = _activity.getLayoutInflater();
+        return inflater.inflate(R.layout.activity_main, null);
+
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------
+    //  Private Members
+    private MainScreen  mMainScreen;
+    private SlideMenu   mLateralMenu;
+}
