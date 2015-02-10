f1509aea85275bab20864af7856d72f17aa67bfc
bardo91 <19Bardo91@gmail.com>
2015-02-10 17:13:59 +0100

    Basic interface to test Hub connection.

--- ui/app/android/app/src/main/java/app/dmc/user_interface/UserInterface.java
+++ ui/app/android/app/src/main/java/app/dmc/user_interface/UserInterface.java
@@ -11,11 +11,11 @@ package app.dmc.user_interface;
 
 import android.app.Activity;
 import android.content.Context;
-import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import app.dmc.Hub;
-import app.dmc.R;
 
 public class UserInterface {
     //-----------------------------------------------------------------------------------------------------------------
@@ -27,6 +27,7 @@ public class UserInterface {
 
     //-----------------------------------------------------------------------------------------------------------------
     public void setHub(Hub _hub){
+        mCurrentHub = _hub;
         mMainScreen.set(_hub);
         mLateralMenu.set(_hub);
     }
@@ -34,8 +35,21 @@ public class UserInterface {
     //-----------------------------------------------------------------------------------------------------------------
     public View build(Context _context){
         // Dummy build
-        LayoutInflater inflater = LayoutInflater.from(_context);
-        return inflater.inflate(R.layout.activity_main, null);
+        /*LayoutInflater inflater = LayoutInflater.from(_context);
+        View xmlView = inflater.inflate(R.layout.activity_main, null);*/
+
+        LinearLayout ll = new LinearLayout(_context);
+        Button b = new Button(_context);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //mCurrentHub.send(JSONObject _json);
+            }
+        });
+        b.setText("Push me");
+        ll.addView(b);
+
+        return  ll;
 
     }
 
@@ -43,4 +57,6 @@ public class UserInterface {
     //  Private Members
     private MainScreen  mMainScreen;
     private SlideMenu   mLateralMenu;
+
+    private Hub mCurrentHub;
 }
