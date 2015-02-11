ef7a663d891cf57fc3e28c606ff49c7ead1fec3f
joscormir <joscormir@gmail.com>
2015-02-11 13:20:33 +0100

    connectClient method added to HubConnection

--- ui/app/android/app/src/main/java/app/dmc/HubConnection.java
+++ ui/app/android/app/src/main/java/app/dmc/HubConnection.java
@@ -1,19 +1,29 @@
 package app.dmc;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 /**
  * Created by Joscormir on 10/02/2015.
  */
 public class HubConnection {
 
-    public static void connectClient(String _ip, int _port) throws Exception{
-        try {
-            Socket clientSocket = new Socket(_ip, _port);
-        }catch(){
+    //First method that connect with Client
 
+    private static void connectClient(String _ip, int _port){
+       try {
+           Socket clientSocket = new Socket(_ip, _port);
+           if(clientSocket.isConnected()) {
+               InputStream inFromServer = clientSocket.getInputStream();
+           }
 
-        }
-
+       }catch(UnknownHostException uhe){
+          System.out.println(uhe.getMessage());
+       }catch(IOException ioe){
+           System.out.println(ioe.getMessage());
+       }
     }
+    //---------------------------------------------------------------------------------------------
 
 }
