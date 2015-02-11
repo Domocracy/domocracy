04661eee79aecf62fdc0f9830439afcd36626aff
technik <cjtech90@gmail.com>
2015-02-11 17:06:09 +0100

    Hue light interface

--- dev/hub/Raspi/dmcServer/src/home/device/hue/hueLight.h
+++ dev/hub/Raspi/dmcServer/src/home/device/hue/hueLight.h
@@ -8,13 +8,21 @@
 #ifndef _DMCSERVER_HOME_DEVICE_HUE_HUELIGHT_H_
 #define _DMCSERVER_HOME_DEVICE_HUE_HUELIGHT_H_
 
-#include <home/device.h>
+#include <home/device/actuator.h>
+#include <home/device/sensor.h>
 
 namespace dmc { namespace hue {
 
-	class Light {
+	class Light : public Actuator, public Sensor {
 	public:
-		Light()
+		Light(unsigned _id, const std::string& _name, const std::string& _hueId);
+
+		// Device interface
+		bool runCommand(const Json& _cmd) override;
+		bool read(const std::string& _param, Json& _dst) override;
+
+	private:
+		std::string mHueId;
 	};
 
 }}	// namespace dmc::hue
---
+++ dev/hub/Raspi/dmcServer/src/home/device/sensor.h
@@ -0,0 +1,24 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Project: DMC Server
+// Date:	2015/Feb/11
+// Author:	Carmelo J. Fdez-Agüera Tortosa
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#ifndef _DMCSERVER_HOME_DEVICE_SENSOR_H_
+#define _DMCSERVER_HOME_DEVICE_SENSOR_H_
+
+#include "../device.h"
+#include <string>
+
+namespace dmc {
+
+	class Json;
+
+	class Sensor : public Device {
+	public:
+		virtual bool read(const std::string& _param, Json& _dst) = 0;
+	};
+}
+
+#endif // _DMCSERVER_HOME_DEVICE_SENSOR_H_
