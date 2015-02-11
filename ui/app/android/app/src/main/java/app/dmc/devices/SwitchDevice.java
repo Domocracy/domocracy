8f7e86f5a7cb81b5426dffcf133b4bc48f3f8d5c
bardo91 <19Bardo91@gmail.com>
2015-02-11 16:37:04 +0100

    Added request to run method of device SwitchDevice to test http request.

--- ui/app/android/app/src/main/java/app/dmc/devices/SwitchDevice.java
+++ ui/app/android/app/src/main/java/app/dmc/devices/SwitchDevice.java
@@ -14,6 +14,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import app.dmc.Hub;
 
 public class SwitchDevice implements  Actuator{
@@ -44,6 +51,44 @@ public class SwitchDevice implements  Actuator{
     @Override
     public void run() {
         /*Send Command*/
+        Thread requestThread = new Thread( new Runnable() {
+            @Override
+            public void run() {
+                String url = "http://10.100.2.45";
+                HttpURLConnection con = null;
+                try {
+                    URL obj = new URL(url);
+
+                    con = (HttpURLConnection) obj.openConnection();
+                    if(con == null)
+                        return;
+                    con.setRequestMethod("GET");
+                    //add request header
+                    //con.setRequestProperty("User-Agent", USER_AGENT);
+                    int responseCode = con.getResponseCode();
+                    System.out.println("\nSending 'GET' request to URL : " + url);
+                    System.out.println("Response Code : " + responseCode);
+
+                    BufferedReader in = new BufferedReader(
+                            new InputStreamReader(con.getInputStream()));
+                    String inputLine;
+                    StringBuffer response = new StringBuffer();
+
+                    while ((inputLine = in.readLine()) != null) {
+                        response.append(inputLine);
+                    }
+                    in.close();
+
+                    //print result
+                    System.out.println(response.toString());
+                }catch (MalformedURLException eMalformed){
+                    eMalformed.printStackTrace();
+                }catch (IOException eIOException){
+                    eIOException.printStackTrace();
+                }
+            }
+        });
+        requestThread.start();
     }
 
     @Override
