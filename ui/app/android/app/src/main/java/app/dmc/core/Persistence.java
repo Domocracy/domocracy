5ef68dd0408a03cc0da3fa9fa6745cd2e3061439
joscormir <joscormir@gmail.com>
2015-02-16 19:09:09 +0100

    Persistence finished, need to check if everything works ok. Now it's implemented but need to review.

--- ui/app/android/app/src/main/java/app/dmc/core/Persistence.java
+++ ui/app/android/app/src/main/java/app/dmc/core/Persistence.java
@@ -1,60 +1,74 @@
 package app.dmc.core;
 
 import android.content.Context;
-import android.provider.MediaStore;
 import android.util.Log;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.jar.JarException;
+
 
 /**
  * Created by Joscormir on 16/02/2015.
  */
 public class Persistence {
     //-----------------------------------------------------------------------------------------------------------------
-    static public void init() { instance = new Persistence(); }
+    static public void init(Context _context) { instance = new Persistence(_context); }
 
     //-----------------------------------------------------------------------------------------------------------------
     public static Persistence get(){ return instance; }
 
     //-----------------------------------------------------------------------------------------------------------------
     public static JSONObject getData(){
-        if(mFile.exists()) {
+        File file = new File(mExternalDirPath + "hubList.json");
+        if(file.exists()){
             try {
-                BufferedReader is = new BufferedReader(new FileReader(mFile));
+                BufferedReader is = new BufferedReader(new FileReader(file));
                 String jsonString = is.readLine();
-                JSONObject json = new JSONObject(jsonString);
+                mJson = new JSONObject(jsonString);
+
             } catch (IOException e) {
                 Log.d("Can't open HubList", e.getMessage());
             }catch (JSONException e){
                 Log.d("Can't open JSONObject", e.getMessage());
             }
-        }
+            return mJson;
+        }else return mJson = null;
     }
 
     //-----------------------------------------------------------------------------------------------------------------
-    public static boolean putData(){
-
-
+    public static boolean putData() throws IOException{
+        File file = new File(mExternalDirPath +"hubList.json");
+        try {
+            FileWriter fileToWrite = new FileWriter(file);
+            BufferedWriter out = new BufferedWriter(fileToWrite);
+            String changesToWrite = mJson.toString();
+            out.write(changesToWrite);
+            out.flush();
+            out.close();
+            return true;
+        } catch (IOException e) {
+            Log.d("Can't open HubList", e.getMessage());
+            return false;
+        }
     }
+
     //-----------------------------------------------------------------------------------------------------------------
 
     //Private interface
     private Persistence(Context _context){
         File externalDir = _context.getFilesDir();
-        String externalDirPath = externalDir.getPath();
-        mFile = new File(externalDirPath + "hubList.json");
+        mExternalDirPath = externalDir.getPath();
+
     }
 
     //-----------------------------------------------------------------------------------------------------------------
-    private static Persistence instance = null;
-    private static File mFile;
+    private static Persistence  instance = null;
+    private static String       mExternalDirPath;
+    private static JSONObject   mJson;
 }
