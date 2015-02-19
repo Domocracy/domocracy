5f781c1aa8fe7daac6d54c76108f40b65102aef0
bardo91 <19Bardo91@gmail.com>
2015-02-19 13:21:53 +0100

    Created scene template layout. WIP: filling Scene class

--- ui/app/android/app/src/main/java/app/dmc/devices/Scene.java
+++ ui/app/android/app/src/main/java/app/dmc/devices/Scene.java
@@ -11,20 +11,45 @@
 package app.dmc.devices;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Map;
 
-public class Scene implements Device{
+import app.dmc.Hub;
+import app.dmc.HubManager;
+import app.dmc.R;
+
+public class Scene implements Actuator{
     //-----------------------------------------------------------------------------------------------------------------
     // Public interface
+    Scene(JSONObject _data){
+        try {
+            mName = _data.getString("name");
+            mId = _data.getString("id");
+            mHubId = _data.getString("hub");
+
+        }catch(JSONException _jsonException) {
+            _jsonException.printStackTrace();
+        }
+    }
 
     //-----------------------------------------------------------------------------------------------------------------
     @Override
     public View view(Context _context) {
-        return null;
+        if(mView == null){
+            LayoutInflater inflater = LayoutInflater.from(_context);
+            LinearLayout base = (LinearLayout) inflater.inflate(R.layout.scene_layout, null);
+
+            
+
+        }
+
+        return mView;
     }
 
     //-----------------------------------------------------------------------------------------------------------------
@@ -40,17 +65,25 @@ public class Scene implements Device{
     }
 
     //-----------------------------------------------------------------------------------------------------------------
+    @Override
+    public void runCommand(JSONObject _jsonCommand) {
+        Hub hub = HubManager.get().hub(mHubId);
+        hub.send("/device/" + id(), _jsonCommand);
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------
     // Private members
 
     // Identification
     private String mName;
     private String mId;
-
+    private String mHubId;
 
     // Content
     private View mView;
 
     private Map<Actuator, JSONObject> mCommands;
+
     //-----------------------------------------------------------------------------------------------------------------
 
     //-----------------------------------------------------------------------------------------------------------------
