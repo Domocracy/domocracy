29a16dcca7b7992f143aafbc542cd24643b055ee
technik <cjtech90@gmail.com>
2015-02-19 20:36:25 +0100

    Implemented setText and operator= in Json

--- dev/hub/Raspi/dmcServer/src/service/user/device/kodi/kodi.cpp
+++ dev/hub/Raspi/dmcServer/src/service/user/device/kodi/kodi.cpp
@@ -7,6 +7,9 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #include "kodi.h"
 #include <core/comm/json/rpc/jsonRpc.h>
+#include <core/comm/http/httpClient.h>
+#include <core/comm/http/request/jsonRequest.h>
+#include <iostream>
 
 namespace dmc { namespace kodi {
 
@@ -19,7 +22,19 @@ namespace dmc { namespace kodi {
 		mIp = _data["ip"].asText();
 
 		// Try to ping Kodi
-		
+		http::Client client;
+		if(!client.connect(mIp, mPort)) {
+			std::cout << "Can't connect to Kodi\n";
+		}
+		else { // Connected
+			std::cout << "Connected to Kodi\n";
+
+			Json notifParams(R"({"title":"DMC", "message": "Domocratizate!"}")");
+			JsonRpcRequest request("GUI.ShowNotification", notifParams, 352);
+
+			http::Response* r = client.makeRequest(http::JsonRequest(http::Request::METHOD::Post, "/jsonrpc", request));
+			std::cout << r->serialize() << "\n";
+		}
 	}
 
 	//------------------------------------------------------------------------------------------------------------------
