54ce4d68f410171123f0f7d167a6bbd717a2ec3c
technik <cjtech90@gmail.com>
2015-02-19 10:44:08 +0100

    Persistence singleton interface

---
+++ dev/hub/Raspi/dmcServer/src/provider/persistence.h
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Project: DMC Server
+// Date:	2015/Feb/11
+// Author:	Carmelo J. Fdez-Agüera Tortosa
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#ifndef _DMCSERVER_PROVIDER_PERSISTENCE_H_
+#define _DMCSERVER_PROVIDER_PERSISTENCE_H_
+
+#include <core/comm/json/json.h>
+#include <string>
+
+namespace dmc {
+
+	class Persistence {
+	public:
+		static Persistence* get		();
+		static void			init	();
+
+		void				saveData(const std::string& _dataId, const Json& _data);
+		Json				getData	(const std::string& _dataId);
+
+	private:
+		Persistence();
+	};
+
+}	// namespace dmc
+
+#endif // _DMCSERVER_PROVIDER_PERSISTENCE_H_
