be933c5858d853b72d83eeb81a8dfe81e124ce3c
technik <cjtech90@gmail.com>
2015-02-20 19:08:52 +0100

    Little readability improvement of user.cpp

--- dev/hub/Raspi/dmcServer/src/service/user/user.cpp
+++ dev/hub/Raspi/dmcServer/src/service/user/user.cpp
@@ -83,15 +83,15 @@ namespace dmc {
 		case Request::METHOD::Put: {
 			// Use device as an actuator
 			Actuator* act = dynamic_cast<Actuator*>(dev);
-			if(!act) {
-				return new Response404(string("Error 404: Device ")+devIdStr+" is not an actuator\n");
-			} else {
+			if(act) {
 				Json body(_request.body()); // Extract body from request
 				bool success = act->runCommand(body);
 				if(success)
 					return new JsonResponse(Json(R"({"result":"ok"})"));
 				else
 					return new JsonResponse(Json(R"({"result":"fail"})"));
+			} else {
+				return new Response404(string("Error 404: Device ")+devIdStr+" is not an actuator\n");
 			}
 		}
 		default:
