e29dac8a55df3208ca7b8216cb4f2ec46736dbf8
bardo91 <19Bardo91@gmail.com>
2015-02-23 17:32:36 +0100

    Dummy scene and its panels

--- ui/app/android/app/src/main/java/app/dmc/devices/Scene.java
+++ ui/app/android/app/src/main/java/app/dmc/devices/Scene.java
@@ -11,109 +11,37 @@
 package app.dmc.devices;
 
 import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.ToggleButton;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.Map;
-
-import app.dmc.Hub;
-import app.dmc.HubManager;
-import app.dmc.R;
-
-public class Scene implements Actuator{
+public class Scene extends Actuator{
     //-----------------------------------------------------------------------------------------------------------------
-    // Public interface
-    public Scene(JSONObject _data, Context _context){
-        try {
-            mName = _data.getString("name");
-            mId = _data.getString("id");
-            mHubId = _data.getString("hub");
-
-        }catch(JSONException _jsonException) {
-            _jsonException.printStackTrace();
-        }
-
-    }
-
-    //-----------------------------------------------------------------------------------------------------------------
+    //  Public Interface
     @Override
-    public View view(Context _context) {
-        if(mView == null){
-            LayoutInflater inflater = LayoutInflater.from(_context);
-            LinearLayout base = (LinearLayout) inflater.inflate(R.layout.scene_layout, null);
+    public DevicePanel createPanel(String _type, JSONObject _panelData, Context _context) {
+        ActuatorPanel huePanel = new ActuatorPanel(this, _panelData, _context) {
+            @Override
+            public void stateChanged(JSONObject _state) {
 
-            // Set name
-            ((TextView) base.findViewById(R.id.devName)).setText(name());
+            }
 
-            // Execute Scene
-            ToggleButton button = (ToggleButton) base.findViewById(R.id.toggleButton);
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Thread t = new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            runCommand(new JSONObject());
-                        }
-                    });
-                    t.start();
-                }
-            });
+            @Override
+            protected void onLayout(boolean changed, int l, int t, int r, int b) {
 
-            // Reconfigure scene
-            base.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View v) {
-                    return false;
-                }
-            });
+            }
+        };
 
-            mView = base;
-
-        }
-
-        return mView;
+        return huePanel;
     }
 
-    //-----------------------------------------------------------------------------------------------------------------
-    @Override
-    public String name() {
-        return mName;
-    }
-
-    //-----------------------------------------------------------------------------------------------------------------
-    @Override
-    public String id() {
-        return mId;
-    }
-
-    //-----------------------------------------------------------------------------------------------------------------
     @Override
     public void runCommand(JSONObject _jsonCommand) {
-        Hub hub = HubManager.get().hub(mHubId);
-        hub.send("/device/" + id(), _jsonCommand);
+
     }
 
-    //-----------------------------------------------------------------------------------------------------------------
-    // Private members
+    @Override
+    public JSONObject action(JSONObject _stateInfo) {
+        return null;
+    }
 
-    // Identification
-    private String mName;
-    private String mId;
-    private String mHubId;
-
-    // Content
-    private View mView;
-
-    private Map<Actuator, JSONObject> mCommands;
-
-    //-----------------------------------------------------------------------------------------------------------------
-
-    //-----------------------------------------------------------------------------------------------------------------
 }
