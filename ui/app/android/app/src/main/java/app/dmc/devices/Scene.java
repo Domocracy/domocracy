d5479e174d3d188271de89b220adfd6b1d29510a
bardo91 <19Bardo91@gmail.com>
2015-02-25 14:46:39 +0100

    Restored scene basics

--- ui/app/android/app/src/main/java/app/dmc/devices/Scene.java
+++ ui/app/android/app/src/main/java/app/dmc/devices/Scene.java
@@ -11,44 +11,63 @@
 package app.dmc.devices;
 
 import android.content.Context;
+import android.view.View;
 
 import org.json.JSONObject;
 
 import app.dmc.R;
 
-public class Scene extends Actuator{
+public class Scene extends Actuator {
     //-----------------------------------------------------------------------------------------------------------------
     //  Public Interface
-    public Scene(JSONObject _sceneData){
+    public Scene(JSONObject _sceneData) {
         super(_sceneData);
     }
 
     //-----------------------------------------------------------------------------------------------------------------
     @Override
     public DevicePanel createPanel(String _type, JSONObject _panelData, Context _context) {
-        ActuatorPanel huePanel = new ActuatorPanel(this, _panelData, R.layout.scene_layout, _context) {
-            @Override
-            public void stateChanged(JSONObject _state) {
-
-            }
-
-            @Override
-            protected void onLayout(boolean changed, int l, int t, int r, int b) {
-
-            }
-        };
-
-        return huePanel;
+        return new ScenePanel(this, _panelData, R.layout.scene_layout, _context);
     }
 
-    @Override
-    public JSONObject runCommand(JSONObject _jsonCommand) {
-        return new JSONObject();
-    }
-
+    //-----------------------------------------------------------------------------------------------------------------
     @Override
     public JSONObject action(JSONObject _stateInfo) {
         return null;
     }
 
+    //-----------------------------------------------------------------------------------------------------------------
+    // Panels
+    class ScenePanel extends ActuatorPanel {
+        public ScenePanel(Actuator _parentActuator, JSONObject _panelData, int _layoutResId, Context _context) {
+            super(_parentActuator, _panelData, _layoutResId, _context);
+
+            setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Thread commThread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            // Put dev in "Sending mode"
+                            // Send Response
+                            JSONObject response = runCommand(new JSONObject());
+
+                            // if(response OK){
+                            //      Dev in mode OK
+                            //else
+                            //      Dev back to last state
+
+                        }
+                    });
+                    commThread.start();
+                }
+            });
+
+        }
+
+        @Override
+        public void stateChanged(JSONObject _state) {
+
+        }
+    }
 }
