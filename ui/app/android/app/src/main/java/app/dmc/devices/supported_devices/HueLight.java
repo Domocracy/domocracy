36fc61f9088bc4c9dc8e0174a8df53ee0969d039
bardo91 <19Bardo91@gmail.com>
2015-02-25 15:12:52 +0100

    Restored HueLight functionality

--- ui/app/android/app/src/main/java/app/dmc/devices/supported_devices/HueLight.java
+++ ui/app/android/app/src/main/java/app/dmc/devices/supported_devices/HueLight.java
@@ -12,8 +12,14 @@ package app.dmc.devices.supported_devices;
 import android.content.Context;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import app.dmc.R;
@@ -44,16 +50,147 @@ public class HueLight extends Actuator {
     //-----------------------------------------------------------------------------------------------------------------
     // Panels
     class HuePanel extends ActuatorPanel{
-        HuePanel(Actuator _parentActuator, JSONObject _panelData, int _layoutResId, Context _context){
+        HuePanel(final Actuator _parentActuator, JSONObject _panelData, int _layoutResId, final Context _context){
             super(_parentActuator, _panelData, _layoutResId, _context);
 
-            ToggleButton button = (ToggleButton) findViewById(R.id.toggleButton);
+            final ToggleButton      button          = (ToggleButton)    findViewById(R.id.toggleButton);
+            TextView                nameView        = (TextView)        findViewById(R.id.devName);
+            SeekBar                 intensityBar   = (SeekBar)         findViewById(R.id.intensityBar);
 
-            button.setOnTouchListener(new OnTouchListener() {
+            // Set dev name
+            nameView.setText(_parentActuator.name());
+
+            // ToggleButton action
+            button.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final JSONObject command = new JSONObject();
+                    try {
+                        command.put("on", button.isChecked());
+                    }catch (JSONException _jsonException){
+                        _jsonException.printStackTrace();
+                    }
+                    Thread commThread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            // Put dev in "Sending mode"
+                            // Send Response
+                            JSONObject response = runCommand(command);
+                            // if(response OK){
+                            //      Dev in mode OK
+                            //else
+                            //      Dev back to last state
+                        }
+                    });
+                    commThread.start();
+
+                }
+            });
+
+            // Intensity Bar actuator.
+            intensityBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                }
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                    final JSONObject command = new JSONObject();
+                    try {
+                        if(seekBar.getProgress()== 0) {
+                            command.put("on", false);
+                        }else {
+                            int intensity = seekBar.getProgress() * 255 / seekBar.getMax();
+                            command.put("on", true);
+                            command.put("bri", intensity);
+                        }
+                    }catch (JSONException _jsonException){
+                        _jsonException.printStackTrace();
+                    }
+
+                    Thread commThread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            // Put dev in "Sending mode"
+                            // Send Response
+                            JSONObject response = runCommand(command);
+                            // if(response OK){
+                            //      Dev in mode OK
+                            //else
+                            //      Dev back to last state
+                        }
+                    });
+                    commThread.start();
+                }
+            });
+
+            // Implementation Expandable View
+            View shortView = findViewById(R.id.shortLayout);
+            shortView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ImageView hueSelector = (ImageView) findViewById(R.id.hueSelector);
+
+                    float iniY = -1;
+                    Animation slideDown = new TranslateAnimation(   Animation.RELATIVE_TO_SELF, 0,
+                            Animation.RELATIVE_TO_SELF, 0,
+                            Animation.RELATIVE_TO_SELF, iniY,
+                            Animation.RELATIVE_TO_SELF, 0);
+                    slideDown.setDuration(400);
+
+                    switch (hueSelector.getVisibility()){
+                        case View.VISIBLE:
+                            hueSelector.setVisibility(View.GONE);
+                            break;
+                        case View.GONE:
+                            hueSelector.setAnimation(slideDown);
+                            hueSelector.setVisibility(View.VISIBLE);
+                            break;
+                    }
+                }
+            });
+
+            // Implementation Hue Selector
+            ImageView hueSelector = (ImageView) findViewById(R.id.hueSelector);
+            hueSelector.setOnTouchListener(new View.OnTouchListener() {
                 @Override
                 public boolean onTouch(View v, MotionEvent event) {
-                    //Log.d("DOMOCRACY", "Touch event on toggle button. X: " + event.getX() + "; Y: " + event.getY());
-                    return false;
+
+                    if(event.getAction() == MotionEvent.ACTION_UP) {
+                        float x = event.getX();
+                        float y = event.getY();
+                        float x0 = v.getX();
+                        float y0 = v.getY();
+                        int hue = (int) ((x - x0)/ v.getWidth() * 65535);
+                        int sat = (int) ((1 - (y - y0) / v.getHeight()) * 255);
+                        final JSONObject command = new JSONObject();
+                        try {
+                            command.put("on", true);
+                            command.put("hue", hue);
+                            command.put("sat", sat);
+                        }catch (JSONException _jsonException){
+                            _jsonException.printStackTrace();
+                        }
+                        Thread commThread = new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                // Put dev in "Sending mode"
+                                // Send Response
+                                JSONObject response = runCommand(command);
+                                // if(response OK){
+                                //      Dev in mode OK
+                                //else
+                                //      Dev back to last state
+                            }
+                        });
+                        commThread.start();
+                    }
+
+                    return true;
                 }
             });
         }
