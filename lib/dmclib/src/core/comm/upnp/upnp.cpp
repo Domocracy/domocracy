da79b767a17851be6abbbc789405cf98d3f9494e
technik <cjtech90@gmail.com>
2015-03-03 17:12:20 +0100

    Candidate: Support for connectionless writes.

--- lib/dmclib/src/core/comm/upnp/upnp.cpp
+++ lib/dmclib/src/core/comm/upnp/upnp.cpp
@@ -20,8 +20,8 @@ namespace dmc {
 	//------------------------------------------------------------------------------------------------------------------
 	void Upnp::discoverServices() {
 		Socket socket;
-		const string cMulticastAddress = "239.255.255.250";
-		bool ok = socket.open(cMulticastAddress, 1900, Socket::Protocol::UDP);
+		const string cMulticastAddress = "localhost"; //"239.255.255.250";
+		bool ok = socket.open(cMulticastAddress, 5900, Socket::Protocol::UDP);
 		if(ok)
 			cout << "Connected to multicast Ip\n";
 		else
@@ -29,11 +29,22 @@ namespace dmc {
 
 		string request = string() + 
 			+"M-SEARCH * HTTP/1.1\r\n"
-			+"HOST: 239.255.255.250:1900\r\n"
+			+"HOST: localhost:5900\r\n"
 			+"MAN: ssdp:discover\r\n"
 			+"MX: 10\r\n"
 			+"ST: ssdp:all\r\n\r\n";
 		cout << request << "\nSending request\n";
+
+		socket.write(request);
+
+		char buffer[64*1024];
+		int nBytes = socket.read(buffer, 64*1024);
+		if(nBytes >= 0) {
+			buffer[nBytes] = 0;
+			cout << "\n>>>\n" << buffer << "\n";
+		} else {
+			cout << "Can't read from UDP\n";
+		}
 
 		//socket.write(request)
 
