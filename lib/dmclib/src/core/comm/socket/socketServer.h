5b495cf64f757b3d1ec238c36448af04fc992c93
technik <cjtech90@gmail.com>
2015-03-06 17:07:14 +0100

    Proper tabulation

--- lib/dmclib/src/core/comm/socket/socketServer.h
+++ lib/dmclib/src/core/comm/socket/socketServer.h
@@ -19,14 +19,14 @@ namespace dmc {
 		~SocketServer();
 
 		typedef std::function<void(Socket*)>	Handler;
-		void setConnectionHandler(Handler _handler);
+		void setConnectionHandler				(Handler _handler);
 
 	private:
-		void		startListening(addrinfo*);
-		void		dispatchConnection(Socket::SocketDesc);
+		void		startListening				(addrinfo*);
+		void		dispatchConnection			(Socket::SocketDesc);
 		void		close();
 
-		addrinfo*	buildAddresInfo			(unsigned _port);
+		addrinfo*	buildAddresInfo				(unsigned _port);
 		Socket::SocketDesc		mListener;
 		bool		mIsListening = false;
 		bool		mMustClose = false;
