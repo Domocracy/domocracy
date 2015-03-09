c88c5862718ff3b76d1988ae854921526dd973f0
bardo91 <19Bardo91@gmail.com>
2015-03-09 16:51:06 +0100

    WIP: Cleaning HueLightPanel.java and ScenePanel.java files

--- ui/app/android/app/src/main/java/app/dmc/devices/supported_devices/philips_hue/HueLightPanel.java
+++ ui/app/android/app/src/main/java/app/dmc/devices/supported_devices/philips_hue/HueLightPanel.java
@@ -14,6 +14,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -30,94 +31,12 @@ public class HueLightPanel extends ActuatorPanel {
     HueLightPanel(final Actuator _parentActuator, JSONObject _panelData, int _layoutResId, final Context _context){
         super(_parentActuator, _panelData, _layoutResId, _context);
 
-        final ToggleButton button          = (ToggleButton)    findViewById(R.id.toggleButton);
-        TextView nameView        = (TextView)        findViewById(R.id.devName);
-        SeekBar intensityBar   = (SeekBar)         findViewById(R.id.intensityBar);
-
+        ToggleButton mToggleButton      = (ToggleButton)    findViewById(R.id.toggleButton);
+        TextView nameView               = (TextView)        findViewById(R.id.devName);
+        SeekBar intensityBar            = (SeekBar)         findViewById(R.id.intensityBar);
+        mExpandButton                   = (Button)          findViewById(R.id.expandViewButton);
         // Set dev name
         nameView.setText(_parentActuator.name());
-
-        // ToggleButton action
-        button.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final JSONObject command = new JSONObject();
-                try {
-                    command.put("on", button.isChecked());
-                }catch (JSONException _jsonException){
-                    _jsonException.printStackTrace();
-                }
-                Thread commThread = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        // Put dev in "Sending mode"
-                        // Send Response
-                        try {
-                            JSONObject cmdRequest = new JSONObject();
-                            cmdRequest.put("method", "PUT");
-                            cmdRequest.put("cmd", command);
-                            JSONObject response = mParentActuator.runCommand(cmdRequest);
-                        } catch (JSONException _jsonException){
-                            _jsonException.printStackTrace();
-                        }
-                        // if(response OK){
-                        //      Dev in mode OK
-                        //else
-                        //      Dev back to last state
-                    }
-                });
-                commThread.start();
-
-            }
-        });
-
-        // Intensity Bar actuator.
-        intensityBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
-            }
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                final JSONObject command = new JSONObject();
-                try {
-                    if(seekBar.getProgress()== 0) {
-                        command.put("on", false);
-                    }else {
-                        int intensity = seekBar.getProgress() * 255 / seekBar.getMax();
-                        command.put("on", true);
-                        command.put("bri", intensity);
-                    }
-                }catch (JSONException _jsonException){
-                    _jsonException.printStackTrace();
-                }
-
-                Thread commThread = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        // Put dev in "Sending mode"
-                        // Send Response
-                        try {
-                            JSONObject cmdRequest = new JSONObject();
-                            cmdRequest.put("method", "PUT");
-                            cmdRequest.put("cmd", command);
-                            JSONObject response = mParentActuator.runCommand(cmdRequest);
-                        } catch (JSONException _jsonException){
-                            _jsonException.printStackTrace();
-                        }
-                        // if(response OK){
-                        //      Dev in mode OK
-                        //else
-                        //      Dev back to last state
-                    }
-                });
-                commThread.start();
-            }
-        });
 
         // Implementation Expandable View
         View shortView = findViewById(R.id.shortLayout);
@@ -193,8 +112,107 @@ public class HueLightPanel extends ActuatorPanel {
         });
     }
 
+    //-----------------------------------------------------------------------------------------------------------------
     @Override
     public void stateChanged(JSONObject _state) {
 
     }
+
+    //-----------------------------------------------------------------------------------------------------------------
+    private void setCallbacks(){
+        // ToggleButton action
+        mToggleButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleButtonCallback();
+            }
+        });
+
+        // Intensity Bar actuator.
+        mIntensityBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                intensityBarCallback();
+            }
+        });
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------
+    private void toggleButtonCallback(){
+        final JSONObject command = new JSONObject();
+        try {
+            command.put("on", mToggleButton.isChecked());
+        }catch (JSONException _jsonException){
+            _jsonException.printStackTrace();
+        }
+        Thread commThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // Put dev in "Sending mode"
+                // Send Response
+                try {
+                    JSONObject cmdRequest = new JSONObject();
+                    cmdRequest.put("method", "PUT");
+                    cmdRequest.put("cmd", command);
+                    JSONObject response = mParentActuator.runCommand(cmdRequest);
+                } catch (JSONException _jsonException){
+                    _jsonException.printStackTrace();
+                }
+                // if(response OK){
+                //      Dev in mode OK
+                //else
+                //      Dev back to last state
+            }
+        });
+        commThread.start();
+    }
+
+    private void intensityBarCallback(){
+        final JSONObject command = new JSONObject();
+        try {
+            if(mIntensityBar.getProgress()== 0) {
+                command.put("on", false);
+            }else {
+                int intensity = mIntensityBar.getProgress() * 255 / mIntensityBar.getMax();
+                command.put("on", true);
+                command.put("bri", intensity);
+            }
+        }catch (JSONException _jsonException){
+            _jsonException.printStackTrace();
+        }
+
+        Thread commThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // Put dev in "Sending mode"
+                // Send Response
+                try {
+                    JSONObject cmdRequest = new JSONObject();
+                    cmdRequest.put("method", "PUT");
+                    cmdRequest.put("cmd", command);
+                    JSONObject response = mParentActuator.runCommand(cmdRequest);
+                } catch (JSONException _jsonException){
+                    _jsonException.printStackTrace();
+                }
+                // if(response OK){
+                //      Dev in mode OK
+                //else
+                //      Dev back to last state
+            }
+        });
+        commThread.start();
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------
+    private Button          mExpandButton;
+    private ToggleButton    mToggleButton;
+    private SeekBar         mIntensityBar;
 }
--- ui/app/android/app/src/main/java/app/dmc/devices/supported_devices/scene/ScenePanel.java
+++ ui/app/android/app/src/main/java/app/dmc/devices/supported_devices/scene/ScenePanel.java
@@ -11,10 +11,12 @@ package app.dmc.devices.supported_devices.scene;
 
 import android.content.Context;
 import android.view.View;
+import android.widget.Button;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import app.dmc.R;
 import app.dmc.devices.Actuator;
 import app.dmc.devices.ActuatorPanel;
 
@@ -22,37 +24,64 @@ public class ScenePanel extends ActuatorPanel {
     public ScenePanel(Actuator _parentActuator, JSONObject _panelData, int _layoutResId, Context _context) {
         super(_parentActuator, _panelData, _layoutResId, _context);
 
-        setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Thread commThread = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        // Put dev in "Sending mode"
-                        // Send Response
-                        try {
-                            JSONObject cmdRequest = new JSONObject();
-                            cmdRequest.put("method", "PUT");
-                            cmdRequest.put("cmd", new JSONObject());
-                            JSONObject response = mParentActuator.runCommand(cmdRequest);
-                        } catch (JSONException _jsonException){
-                            _jsonException.printStackTrace();
-                        }
-                        // if(response OK){
-                        //      Dev in mode OK
-                        //else
-                        //      Dev back to last state
+        mExpandButton = (Button) findViewById(R.id.expandViewButton);
 
-                    }
-                });
-                commThread.start();
-            }
-        });
-
+        setCallbacks();
     }
 
+    //-----------------------------------------------------------------------------------------------------------------
     @Override
     public void stateChanged(JSONObject _state) {
 
     }
+
+    //-----------------------------------------------------------------------------------------------------------------
+    // Private members
+    private void onClickRunCommand(){
+        Thread commThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // Put dev in "Sending mode"
+                // Send Response
+                try {
+                    JSONObject cmdRequest = new JSONObject();
+                    cmdRequest.put("method", "PUT");
+                    cmdRequest.put("cmd", new JSONObject());
+                    JSONObject response = mParentActuator.runCommand(cmdRequest);
+                } catch (JSONException _jsonException){
+                    _jsonException.printStackTrace();
+                }
+                // if(response OK){
+                //      Dev in mode OK
+                //else
+                //      Dev back to last state
+
+            }
+        });
+        commThread.start();
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------
+    private void setCallbacks(){
+        // Generic click callback
+        setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickRunCommand();
+            }
+        });
+
+        mExpandButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Expand View
+            }
+        });
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------
+    //
+
+    private Button mExpandButton;
+
 }
