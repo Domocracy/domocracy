06c9b704c7809884cd06dac0da93baf80f0fc88a
joscormir <joscormir@gmail.com>
2015-03-10 18:15:21 +0100

    added room_header.xml and its photo to upload a default image.

--- ui/app/android/app/src/main/java/app/dmc/user_interface/RoomHeader.java
+++ ui/app/android/app/src/main/java/app/dmc/user_interface/RoomHeader.java
@@ -10,18 +10,20 @@
 package app.dmc.user_interface;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.widget.LinearLayout;
-import android.widget.TextView;
+
+import app.dmc.R;
 
 public class RoomHeader extends LinearLayout{
     //-----------------------------------------------------------------------------------------------------------------
     public RoomHeader(Context _context){
         super(_context);
 
-        TextView titleView = new TextView(_context);
-        titleView.setText("HEADER");
-        addView(titleView);
-    }
+        LayoutInflater inflater = LayoutInflater.from(_context);
+        addView(inflater.inflate(R.layout.room_header, null));
+        
+        }
     //-----------------------------------------------------------------------------------------------------------------
 
     //-----------------------------------------------------------------------------------------------------------------
