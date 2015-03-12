45c114f531a94cd81ca49da525ace4cce08eb8b6
bardo91 <19Bardo91@gmail.com>
2015-03-12 13:43:05 +0100

    Filled new scene menu with hub devices.

--- ui/app/android/app/src/main/java/app/dmc/user_interface/NewSceneMenu.java
+++ ui/app/android/app/src/main/java/app/dmc/user_interface/NewSceneMenu.java
@@ -14,6 +14,14 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import app.dmc.User;
 
 public class NewSceneMenu extends DialogFragment{
     public NewSceneMenu(){
@@ -23,7 +31,7 @@ public class NewSceneMenu extends DialogFragment{
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Load devices and put them into the list
-
+        setContentView();
         // Set add button
         mMenuBuilder.setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
@@ -37,13 +45,31 @@ public class NewSceneMenu extends DialogFragment{
 
     //-----------------------------------------------------------------------------------------------------------------
     // Private methods
-    private void addScene(){
+    private void setContentView(){
+        LinearLayout layout = new LinearLayout(getActivity());
+        TextView title = new TextView(getActivity());
+        title.setText("Add new scene");
+        layout.addView(title);
 
+        mDevList = new ListView(getActivity());
+        List<String> devicesId = User.get().getCurrentHub().devicesId();
+
+        for(int i = 0; i < devicesId.size(); i++){
+            CheckBox devCheckBox = new CheckBox(getActivity());
+            devCheckBox.setText(User.get().getCurrentHub().device(devicesId.get(i)).name());
+        }
+
+        mMenuBuilder.setView(layout);
+    }
+
+    private void addScene(){
+        
     }
 
     //-----------------------------------------------------------------------------------------------------------------
     // Private members
     private AlertDialog.Builder mMenuBuilder;
+    ListView mDevList;
 
 
 }
