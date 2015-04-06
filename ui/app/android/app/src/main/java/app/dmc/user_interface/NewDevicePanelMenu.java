59e094ba074b2b259bce2e82bee135b6a78357cd
Bardo91 <19Bardo91@gmail.com>
2015-04-06 16:39:21 +0200

    Completed new panel menu and added panel to room.

--- ui/app/android/app/src/main/java/app/dmc/user_interface/NewDevicePanelMenu.java
+++ ui/app/android/app/src/main/java/app/dmc/user_interface/NewDevicePanelMenu.java
@@ -11,13 +11,19 @@ package app.dmc.user_interface;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.util.Pair;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import app.dmc.Room;
 import app.dmc.User;
+import app.dmc.devices.Device;
+import app.dmc.devices.DevicePanel;
 
 public class NewDevicePanelMenu {
     public NewDevicePanelMenu(Context _context, Room _room){
@@ -38,13 +44,20 @@ public class NewDevicePanelMenu {
     }
 
     //-----------------------------------------------------------------------------------------------------------------
-    private void setContentView(Context _context){
+    private void setContentView(final Context _context){
         LinearLayout layout = new LinearLayout(_context);
         layout.setOrientation(LinearLayout.VERTICAL);
         List<String> devices = User.get().getCurrentHub().deviceIds();
-        for(String id :devices){
+        for(final String id :devices){
             TextView tv = new TextView(_context);
             tv.setText(User.get().getCurrentHub().device(id).name());
+            tv.setTextSize(30);
+            tv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    PanelListMenu menu = new PanelListMenu(_context, mParentRoom, User.get().getCurrentHub().device(id));
+                }
+            });
             layout.addView(tv);
         }
         mMenuBuilder.setView(layout);
@@ -54,4 +67,61 @@ public class NewDevicePanelMenu {
     // Private members
     private AlertDialog.Builder mMenuBuilder;
     private Room                mParentRoom;
+
+
+    //-----------------------------------------------------------------------------------------------------------------
+    // Inner classes
+    class PanelListMenu{
+        public PanelListMenu(Context _context, Room _room, Device _device){
+            mMenuBuilder = new AlertDialog.Builder(_context);
+            mParentRoom = _room;
+            mDevice = _device;
+            mPanels =  new ArrayList<>();
+
+            createDialog(_context);
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------
+        // Private methods
+        private void createDialog(Context _context) {
+            // Load devices and put them into the list
+            setContentView(_context);
+            mMenuBuilder.setTitle("Choose controller to be added:");
+
+            mMenuBuilder.create().show();;
+
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------
+        private void setContentView(final Context _context){
+            List<Pair<String, Boolean>> types = mDevice.panelTypes();
+            LinearLayout layout = new LinearLayout(_context);
+            layout.setOrientation(LinearLayout.VERTICAL);
+            for(int i = 0; i < types.size(); i++){
+                final String type = types.get(i).first;
+                DevicePanel panel = User.get().getCurrentHub().device(mDevice.id()).newPanel(type, _context);
+                panel.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        if(event.getAction() == MotionEvent.ACTION_UP) {
+                            mParentRoom.addPanel(User.get().getCurrentHub().device(mDevice.id()).newPanel(type, _context));
+
+                        }
+                        return true;
+                    }
+                });
+                mPanels.add(panel);
+                layout.addView(panel);
+            }
+            mMenuBuilder.setView(layout);
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------
+        // Private members
+        private AlertDialog.Builder mMenuBuilder;
+        private Room                mParentRoom;
+        private Device              mDevice;
+        private ArrayList<DevicePanel> mPanels;
+    }
+
 }
