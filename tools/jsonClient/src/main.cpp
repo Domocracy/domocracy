055171052f31dbad48aeed0eeeea0d27a8114a29
Bardo91 <19Bardo91@gmail.com>
2015-06-02 15:03:39 +0200

    Codded main loop. First version of JsonClient tool.

--- tools/jsonClient/src/main.cpp
+++ tools/jsonClient/src/main.cpp
@@ -8,8 +8,37 @@
 //
 
 
-int main(int _argc, char ** _argv){
+//	Tool to test json dmc server.
+//	
+//	Arguments:
+//		1. Host's url.
+//		2. Server's port.
+//
 
+#include "JsonClient.h"
+
+#include <iostream>
+
+int main(int _argc, char ** _argv){
+	// Check input arguments.
+	if (_argc < 2){
+		std::cout << "Not enough input arguments" << std::endl;
+		return -1;
+	}
+
+	// Decode args.
+	std::string urlHost(_argv[1]);
+	unsigned	port = atoi(_argv[2]);
+
+	// Init Client.
+	dmc_tools::JsonClient client(urlHost, port);
+
+	// Main loop.
+	std::string stream;
+	do{
+		std::cin >> stream;
+		client.send(dmc::Json(stream));
+	} while (strcmp(stream.c_str(), "EXIT") != 0 && client.isConnected());
 
 
 }
